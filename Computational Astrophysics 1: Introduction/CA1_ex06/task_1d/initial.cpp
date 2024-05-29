efa1d227b58064daa13180dd6d60cb481a7835fd
astro-seha <108924295+AstroSeha@users.noreply.github.com>
2024-05-30 00:45:56 +0200

    task 1 done

--- Computational Astrophysics 1: Introduction/CA1_ex06/task_1d/initial.cpp
+++ Computational Astrophysics 1: Introduction/CA1_ex06/task_1d/initial.cpp
@@ -3,16 +3,18 @@
 
 using namespace std;
 
-void initial(double& x0, double& y0, double& vx0, double& vy0, double& dt) {
-    y0 = 0.0;
-    vx0 = 0.0;
+void initial(double& x0, double& T, double& a_initial) {
+ 
+    cout << "Orbital period[year]: ";
+    cin >> T;
     
-    cout << "Size of the time step (dt): ";
-    cin >> dt;
-    
-    cout << "Initial value x(t = 0): ";
+    // semi-major axis from Kepler's third law, au and year
+    a_initial = pow(T, 2./3);
+
+    cout << "Initial value of x[au], when t=y=0: ";
     cin >> x0;
+
+    // vis viva equation when GM=4*pi^2
+    double vy0 = 2*M_PI *sqrt(2./x0 - 1./a_initial); 
     
-    cout << "Initial value vy(t = 0): ";
-    cin >> vy0;
 }
