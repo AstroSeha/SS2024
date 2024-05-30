2233e7b0fb3bbc955b1ca5d4514c735d1ce7eb7d
astro-seha <108924295+AstroSeha@users.noreply.github.com>
2024-05-30 10:56:58 +0200

    rel_change_array

--- Computational Astrophysics 1: Introduction/CA1_ex06/task_2/period.cpp
+++ Computational Astrophysics 1: Introduction/CA1_ex06/task_2/period.cpp
@@ -35,26 +35,5 @@ double theoretical_period(double x[], double y[], int N, double dt) {
     } else {
         return pow(r2, 1.5);
     }
-}
 
-// save the results
-void log_results(double x0, double y0, double vx0, double vy0, double dt, int nout, double experimental_period, double theoretical_period) {
-    ofstream logfile("period_log.txt", ios_base::app);
-    if (!logfile.is_open()) {
-        cerr << "Unable to open log file" << endl;
-        return;
-    }
-
-    // get current time
-    time_t now = time(0);
-    char* dt_now = ctime(&now);
-
-    logfile << "Timestamp: " << dt_now;
-    logfile << "Initial conditions: x0 = " << x0 << ", y0 = " << y0 << ", vx0 = " << vx0 << ", vy0 = " << vy0 << endl;
-    logfile << "Time step: " << dt << ", nout: " << nout << endl;
-    logfile << "Experimental period: " << experimental_period << " years" << endl;
-    logfile << "Theoretical period: " << theoretical_period << " years" << endl;
-    logfile << "-----------------------------------------" << endl;
-
-    logfile.close();
 }
