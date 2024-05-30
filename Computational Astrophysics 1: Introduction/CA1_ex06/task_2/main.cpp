6aa3179cc9d0d0251f00062042c7075f7a0d8e23
astro-seha <108924295+AstroSeha@users.noreply.github.com>
2024-05-30 11:16:19 +0200

    gnuplot

--- Computational Astrophysics 1: Introduction/CA1_ex06/task_2/main.cpp
+++ Computational Astrophysics 1: Introduction/CA1_ex06/task_2/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <fstream>
 #include "euler.cpp"
 #include "euler_cromer.cpp"
 #include "euler_richardson.cpp"
@@ -10,14 +11,13 @@
 
 using namespace std;
 
-void run_simulation(void (*method)(double[], double[], double[], double[], double[], int, double), double& rel_change, double x0, double vy0, double dt, double T) {
+void run_simulation(void (*method)(double[], double[], double[], double[], double[], int, double), double rel_change[], double t[], double x0, double vy0, double dt, double T) {
     int N = 100 * T / dt;
     double* x = new double[N];
     double* y = new double[N];
     double* vx = new double[N];
     double* vy = new double[N];
     double* r = new double[N];
-    double* rel_change_array = new double[N];
 
     x[0] = x0;
     y[0] = 0.0;
@@ -29,10 +29,9 @@ void run_simulation(void (*method)(double[], double[], double[], double[], doubl
     method(x, y, vx, vy, r, N, dt);
 
     for (int i = 0; i < N; i++) {
-        rel_change_array[i] = abs(energy(x[i], y[i], vx[i], vy[i]) - E_initial) / E_initial;
-    }  
-    rel_change = rel_change_array[N-1];
-
+        rel_change[i] = abs((energy(x[i], y[i], vx[i], vy[i]) - E_initial) / E_initial);
+        t[i] = i * dt;
+    }
 
     delete[] x;
     delete[] y;
@@ -42,25 +41,37 @@ void run_simulation(void (*method)(double[], double[], double[], double[], doubl
 }
 
 int main() {
-    double x0 = 1.0;
-    double vy0 = 2 * M_PI;
-    double dt = 1e-3;
+    double x0, vy0, dt;
 
     initial(x0, vy0, dt);
-    double T = theoretical_period(x0, vy0);
+    double T = theoretical_period(x0, vy0, N, dt);
+    int N = 100 * T / dt;
 
-    double rel_change_euler, rel_change_euler_cromer, rel_change_euler_richardson, rel_change_rk4;
+    double* rel_change_euler = new double[N];
+    double* rel_change_euler_cromer = new double[N];
+    double* rel_change_euler_richardson = new double[N];
+    double* rel_change_rk4 = new double[N];
+    double* t = new double[N];
 
-    run_simulation(euler, rel_change_euler, x0, vy0, dt, T);
-    run_simulation(euler_cromer, rel_change_euler_cromer, x0, vy0, dt, T);
-    run_simulation(euler_richardson, rel_change_euler_richardson, x0, vy0, dt, T);
-    run_simulation(runge_kutta, rel_change_rk4, x0, vy0, dt, T);
+    run_simulation(euler, rel_change_euler, t, x0, vy0, dt, T);
+    run_simulation(euler_cromer, rel_change_euler_cromer, t, x0, vy0, dt, T);
+    run_simulation(euler_richardson, rel_change_euler_richardson, t, x0, vy0, dt, T);
+    run_simulation(runge_kutta, rel_change_rk4, t, x0, vy0, dt, T);
 
-    cout << "Relative change in energy after 100 orbits:" << endl;
-    cout << "Euler: " << rel_change_euler << endl;
-    cout << "Euler-Cromer: " << rel_change_euler_cromer << endl;
-    cout << "Euler-Richardson: " << rel_change_euler_richardson << endl;
-    cout << "RK4: " << rel_change_rk4 << endl;
+    ofstream outfile;
+    outfile.open("accuracy.dat");
+    for (int i = 0; i < N; i++) {
+        outfile << t[i] << " " << rel_change_euler[i] << " " << rel_change_euler_cromer[i] << " " << rel_change_euler_richardson[i] << " " << rel_change_rk4[i] << endl;
+    }
+    outfile.close();
+
+    delete[] rel_change_euler;
+    delete[] rel_change_euler_cromer;
+    delete[] rel_change_euler_richardson;
+    delete[] rel_change_rk4;
+    delete[] t;
+
+    cout << "Data saved to accuracy.dat" << endl;
 
     return 0;
 }
